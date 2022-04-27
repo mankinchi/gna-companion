14fd4b59792653b857f20e625ccdfb3645a50163
Tri Nguyen <mankinchi.1995@gmail.com>
2022-04-27 14:38:24 +1000

    add basic pages

---
+++ components/adventures/theGrandTournaments/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const TheGrandTournament = () => {
+	console.log('123');
+	return (
+		<>
+			<Head>
+				<title>The Grand Tournament</title>
+			</Head>
+			<div>Name: Tri</div>
+			<div>Health</div>
+			<div>Codewords</div>
+			<div>Special Techniques</div>
+			<div>Other special techniques</div>
+			<div>Bugs</div>
+		</>
+	);
+};
+
+export default TheGrandTournament;
